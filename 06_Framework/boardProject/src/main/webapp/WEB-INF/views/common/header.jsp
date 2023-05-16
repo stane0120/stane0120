7051678afdae7fd01804da192dcb3d0e6421c798
stane0120 <stane0120@iei.or.kr>
2023-05-16 15:11:03 +0900

    20230516 학원에서 커밋 1

--- 06_Framework/boardProject/src/main/webapp/WEB-INF/views/common/header.jsp
+++ 06_Framework/boardProject/src/main/webapp/WEB-INF/views/common/header.jsp
@@ -112,5 +112,12 @@
             </li>
 
         </c:forEach>
+
+        <%-- 로그인 했을 때 채팅 보여짐 --%>
+        <c:if test="${not empty loginMember}" >
+            <li><a href="/chatting">채팅</a></li>
+        </c:if>
+
+
     </ul>
 </nav>
--- 06_Framework/boardProject/src/main/webapp/resources/js/main.js
+++ 06_Framework/boardProject/src/main/webapp/resources/js/main.js
@@ -4,7 +4,7 @@ const loginFrm = document.getElementById("loginFrm");
 const memberEmail = document.querySelector("#loginFrm input[name='memberEmail']");
 const memberPw = document.querySelector("#loginFrm input[name='memberPw']");
 
-
+if(loginFrm != null){
 // 로그인 시도를 할 때
 loginFrm.addEventListener("submit", e => {
 
@@ -39,7 +39,7 @@ loginFrm.addEventListener("submit", e => {
 
 
 });
-
+}
 
 // 비동기로 이메일이 일치하는 회원의 닉네임을 조회
 function selectNickname(email) {
@@ -192,3 +192,36 @@ btn3.addEventListener("click", () => {
         console.log(err);
     });
 });
+
+
+// -----------------------------------------------------------------------------
+// 웹소켓 테스트
+// 1. SockJS 라이브러리 추가
+
+// 2. SockJS를 이용해서 클라이언트용 웹소켓 객체 새엇ㅇ
+let testSock = new SockJS("/testSock");
+
+function sendMessage(name, str){
+
+    // 매개변수를 JS객체에 저장
+    let obj = {}; // 비어있는 객체
+
+    obj.name = name; // 객체에 일치하는 key가 없다면 자동으로 추가
+    obj.str = str;
+
+    // console.log(obj);
+
+    // 웹소켓 연결된 곳으로 메시지를 보냄
+    testSock.send(JSON.stringify(obj)); 
+                    // JS객체 -> JSON
+}
+
+// 웹소켓 객체(testSock)가 서버로 부터 전달 받은 메시지가 있을 경우
+testSock.onmessage = e => {
+    // e : 이벤트 객체
+    // e.data : 전달 받은 메세지 (JSON)
+
+    let obj = JSON.parse(e.data); // JSON -> JS 객체
+
+    console.log(`보낸사람 : ${obj.name} / ${obj.str}`);
+}
